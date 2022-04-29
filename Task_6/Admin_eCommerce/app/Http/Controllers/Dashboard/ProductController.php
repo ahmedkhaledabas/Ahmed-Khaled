c0aab6abff09c8a6e10cfe82cb9331d1d98ef95e
ahmedkhaledabas <ahmedabasshalby@gmail.com>
2022-04-29 14:56:07 +0200

    update

--- Task_6/Admin_eCommerce/app/Http/Controllers/Dashboard/ProductController.php
+++ Task_6/Admin_eCommerce/app/Http/Controllers/Dashboard/ProductController.php
@@ -8,19 +8,56 @@ use App\Http\Controllers\Controller;
 
 class ProductController extends Controller
 {
-    public const statues = ['Not Active','Active'];
-    public function index(){
+    public const statues = ['Not Active', 'Active'];
+    public const max_upload_size = 1024;
+    public const available_extension = ['png', 'jpg', 'jpeg'];
+    public function index()
+    {
         $products = DB::table('products')->get();
-        return view('Products.index',compact('products'));
+        return view('Products.index', compact('products'));
     }
 
-    public function create(){
-        $brands = DB::table('brands')->select('id','name_en','name_ar')->get();
-        $subcategories = DB::table('sub_category')->select('id','name_en','name_ar')->get();
-      return view('Products.Create',compact('brands','subcategories'))->with('statues',self::statues);
+    public function create()
+    {
+        $brands = DB::table('brands')->select('id', 'name_en', 'name_ar')->get();
+        $subcategories = DB::table('sub_category')->select('id', 'name_en', 'name_ar')->get();
+        return view('Products.Create', compact('brands', 'subcategories'))->with('statues', self::statues);
     }
 
-    public function store(){
-        return view('');
-      }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name_en' => 'required|string|max:50',
+            'name_ar' => 'required|string|max:50',
+            'price'   => 'required|numeric|between:1,99999.99',
+            'quantity' => 'nullable|integer|between:1,99',
+            'code'    => 'required|max:11|unique:products',
+            'statues' => 'nullable|in' . implode(',', array_keys(self::statues)),
+            'brand_id' => 'nullable|integer|exists:brands,id',
+            'subcategory_id' => 'nullable|integer|exists:sub_category,id',
+            'desc_en' => 'required|string',
+            'desc_ar' => 'required|string',
+            'image'   => 'required|max:' . self::max_upload_size . ''
+
+        ]);
+        $data = $request->only('name_en', 'name_ar', 'price', 'quantity', 'desc_en', 'image', 'desc_ar', 'code', 'status', 'brand_id', 'subcategory_id');
+        // dd($data);die;
+
+        $picName = $request->file('image')->hashName();
+        $data['image'] = $picName;
+        $request->file('image')->move(public_path('assets/images/products'), $picName);
+        DB::table('products')->insert($data);
+        if ($request->has('redirect') && $request->redirect == 'index') {
+            return redirect()->route('dashboard.products.index');
+        }
+        return redirect()->back();
+
+        // return view('');
+
+    }
+
+    public function edit($id)
+    {
+        $product = DB::table('products')->where('id', $id)->get();
+    }
 }
--- Task_6/Admin_eCommerce/resources/views/Products/Create.blade.php
+++ Task_6/Admin_eCommerce/resources/views/Products/Create.blade.php
@@ -13,20 +13,27 @@
         <!-- /.card-header -->
         <div class="card-body">
             <form action="{{ route('dashboard.products.store') }}" method="post">
+                @csrf
                 <div class="row">
                     <div class="col-sm-6">
                         <!-- textarea -->
                         <div class="form-group">
                             <label>Name En</label>
-                            <input name="name_en" id="name_en" type="text" class="form-control"
-                                placeholder="Enter Name En">
+                            <input name="name_en" id="name_en" type="text" class="form-control" placeholder="Enter Name En"
+                                value="{{ old('name_en') }}">
+                            @error('name_en')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label>Name Ar</label>
                             <input name="name_ar" id="name_ar" type="text" class="form-control"
-                                placeholder="Enter Name Ar">
+                                placeholder="Enter Name Ar" value="{{ old('name_ar') }}">
+                            @error('name_ar')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -35,21 +42,32 @@
                     <div class="col-sm-4">
                         <div class="form-group">
                             <label>Code</label>
-                            <input name="code" id="code" type="number" class="form-control" placeholder="Enter Code">
+                            <input name="code" id="code" type="number" class="form-control" placeholder="Enter Code"
+                                value="{{ old('code') }}">
+                            @error('code')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                     <div class="col-sm-4">
                         <div class="form-group">
                             <label>Quantity</label>
                             <input name="quantity" id="quantity" type="number" class="form-control"
-                                placeholder="Enter Quantity">
+                                placeholder="Enter Quantity" value="{{ old('quantity') }}">
+                            @error('quantity')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-sm-4">
                         <div class="form-group">
                             <label>Price</label>
-                            <input name="price" id="price" type="number" class="form-control" placeholder="Enter Price">
+                            <input name="price" id="price" type="number" class="form-control" placeholder="Enter Price"
+                                value="{{ old('price') }}">
+                            @error('price')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
 
@@ -62,10 +80,14 @@
                             <select name="status" id="status" class="form-control">
                                 <option>----Select Statues----</option>
                                 @foreach ($statues as $value => $status)
-                                    <option value="{{ $value }}"> {{ $status }} </option>
+                                    <option @selected(old('status') == $value) value="{{ $value }}"> {{ $status }}
+                                    </option>
                                 @endforeach
 
                             </select>
+                            @error('status')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                     <div class="col-sm-4">
@@ -74,10 +96,14 @@
                             <select name="brand_id" id="brand_id" class="form-control">
                                 <option>----Select Brand----</option>
                                 @foreach ($brands as $brand)
-                                    <option value="{{ $brand->id }}"> {{ $brand->name_en }} </option>
+                                    <option @selected(old('brand_id') == $brand->id) value="{{ $brand->id }}">
+                                        {{ $brand->name_en }} </option>
                                 @endforeach
 
                             </select>
+                            @error('brand_id')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                     <div class="col-sm-4">
@@ -86,10 +112,14 @@
                             <select name="subcategory_id" id="subcategory_id" class="form-control">
                                 <option>----Select SubCategory----</option>
                                 @foreach ($subcategories as $subcategory)
-                                    <option value="{{ $subcategory->id }}"> {{ $subcategory->name_en }} </option>
+                                    <option @selected(old('subcategory_id') == $subcategory->id) value="{{ $subcategory->id }}">
+                                        {{ $subcategory->name_en }} </option>
                                 @endforeach
 
                             </select>
+                            @error('subcategory_id')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -99,14 +129,20 @@
                         <div class="form-group">
                             <label>Desc En</label>
                             <textarea rows="4" name="desc_en" id="desc_en" type="text" class="form-control"
-                                placeholder="Enter Name En"></textarea>
+                                placeholder="Enter Name En">{{ old('desc_en') }}</textarea>
+                            @error('desc_en')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label>Desc Ar</label>
                             <textarea rows="4" name="desc_ar" id="desc_ar" type="text" class="form-control"
-                                placeholder="Enter Name Ar"></textarea>
+                                placeholder="Enter Name Ar">{{ old('desc_ar') }}</textarea>
+                            @error('desc_ar')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -117,7 +153,10 @@
                                 <img id="image" style="cursor: pointer;" width="200px" height="200px"
                                     src="{{ asset('assets/Images/upload.png') }}" alt="upload photo">
                             </label>
-                            <input class="d-none" type="file" name="img" id="img" onchange="loadfile(event)">
+                            <input class="d-none" type="file" name="image" id="img" onchange="loadFile(event)">
+                            @error('image')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -125,8 +164,8 @@
                 <div class="row">
                     <div class="col-6">
                         <div class="form-group">
-                            <button class="btn btn-primary">Create</button>
-                            <button class="btn btn-primary">Create & Return</button>
+                            <button class="btn btn-primary" name="redirect" value="index">Create</button>
+                            <button class="btn btn-primary" name="redirect" value="back">Create & Return</button>
 
                         </div>
                     </div>
